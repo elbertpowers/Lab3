6135bbfa0c644ab92ebd677f10020266b11f264a
elbertpowers <sopowers@mail.com>
2016-09-21 14:22:19 -0400

    Lab 3

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -10,6 +10,26 @@ namespace Lab3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Learn your Squares and Cubes!!\n");
+            string ans_cont;
+            do
+            {
+                Console.Write("Enter an Integer:");
+                string numberInput = Console.ReadLine();
+                int number = int.Parse(numberInput);
+
+                for (int count = 1; count <= number; count++)
+                { Console.WriteLine(count.ToString() + " " + (count * count).ToString() + " " + (count * count * count).ToString()); }
+                //hello
+                Console.WriteLine("Continue?");
+                Console.WriteLine("Enter yes or no");
+                ans_cont = Console.ReadLine();
+            } while (ans_cont == "yes");
+            if (ans_cont == "no") ;
+            {
+                Console.WriteLine("Good bye");
+            }
+
         }
     }
 }
